4b556240d3580ec3c50b0fab6f8c7f4b311dfdfa
neha8307 <nehachauhan110801@gmail.com>
2026-04-05 09:13:05 -0500

    Sum of two numbers

--- two_sum.py
+++ two_sum.py
@@ -1,12 +1,8 @@
- def twoSum(nums, target):
-    num_map = {}   # create empty dictionary
+nums = [2, 7, 11, 15]
+target = 9
 
-    for i, num in enumerate(nums):
-        complement = target - num
-
-        if complement in num_map:
-            return [num_map[complement], i]
-
-        num_map[num] = i
-
-        print(twoSum(nums, target))
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            print("Numbers are:", nums[i], "and", nums[j])
+            print("Index position:", i, "and", j)
